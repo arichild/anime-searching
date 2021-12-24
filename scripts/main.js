ecb077916cf1e8217a7f27be325180841074a8e6
arichild <arieevq@gmail.com>
2021-12-24 09:32:45 +0300

    save

---
+++ scripts/main.js
@@ -0,0 +1,62 @@
+const mainUrl = 'https://kitsu.io/api/edge';
+
+window.addEventListener('load', pageLoaded);
+
+function searchAnime(e) {
+  e.preventDefault();
+
+  const form = new FormData(this);
+  const query = form.get('search-anime');
+
+  if(query.length !== 0) {
+    fetch(`${mainUrl}/anime?filter[text]=${query}`) 
+    .then(res => res.json())
+    .then(updDom)
+    .catch(err => console.warm(err.message))
+  } 
+}
+
+function updDom(res) {
+  const searchResult = document.getElementById('result');
+
+  const animeByCategories = res.data
+    .reduce((acc, anime) => {
+        const {showType} = anime.attributes;
+        
+        if (acc[showType] === undefined) 
+        acc[showType] = [];
+        acc[showType].push(anime);
+
+        return acc;
+
+    }, {});
+
+  searchResult.innerHTML = Object.keys(animeByCategories).map(key => {
+      const animesHTML = animeByCategories[key]
+        .sort((a, b) => a.attributes.id-b.attributes.id)
+        .map(item => {
+          console.log(item)
+
+          return `
+            <div class='card'>
+              <img src='${item.attributes.posterImage.original}'>
+              <div class='name'>${item.attributes.titles.en_jp}</div>
+              <div class='episodes'>Episodes: ${item.attributes.episodeCount}</div>
+              <div class='description'>${item.attributes.synopsis}</div>
+            </div>
+          `;
+      }).join('');
+
+      return `
+        <section>
+            <h3>${key.toUpperCase()}</h3>
+            <div class="row">${animesHTML}</div>
+        </section>
+      `
+    }).join('');
+}
+
+function pageLoaded() {
+  const form = document.getElementById('search-form');
+  form.addEventListener('submit', searchAnime)
+}
