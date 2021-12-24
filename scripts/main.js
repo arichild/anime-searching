eb0c23e41f013afd9242c5c552ca80ee1659cb4c
arichild <arieevq@gmail.com>
2021-12-24 19:21:25 +0300

    save

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@ const mainUrl = 'https://kitsu.io/api/edge';
 window.addEventListener('load', pageLoaded);
 
 function searchAnime(e) {
-  e.preventDefault();
-
   const form = new FormData(this);
   const query = form.get('search-anime');
+
+  e.preventDefault();
 
   if(query.length !== 0) {
     fetch(`${mainUrl}/anime?filter[text]=${query}`) 
@@ -18,7 +18,6 @@ function searchAnime(e) {
 
 function updDom(res) {
   const searchResult = document.getElementById('result');
-
   const animeByCategories = res.data
     .reduce((acc, data) => {
         const {showType} = data.attributes;
@@ -28,7 +27,6 @@ function updDom(res) {
         acc[showType].push(data);
 
         return acc;
-
     }, {});
 
   searchResult.innerHTML = Object.keys(animeByCategories).map(key => {
@@ -36,25 +34,23 @@ function updDom(res) {
         .sort((a, b) => a.attributes.id-b.attributes.id)
         .map(item => {
 
-          if(item.attributes.episodeCount === null) {
+          if (item.attributes.episodeCount === null) {
             item.attributes.episodeCount = 0;
           }
 
           return `
-          <div class='card'>
-            <div class="test">  
+            <div class='card'>
               <div class="img-box">
-              <img src='${item.attributes.posterImage.original}'>
-              <div class="rating">${Math.floor(item.attributes.averageRating * 10) / 100}</div>
+                <img src='${item.attributes.posterImage.original}'>
+                <div class="rating">${Math.floor(item.attributes.averageRating * 10) / 100}</div>
               </div>
               <div class='name'>${item.attributes.titles.en_jp}</div>
               <div class='episodes'>Episodes: ${item.attributes.episodeCount}</div>
               <div class='description'>${item.attributes.synopsis}</div>
-              <div class="testq">
+              <div class="trailer-btn">
                 <a href="https://www.youtube.com/watch?v=${item.attributes.youtubeVideoId}">
                   Watch trailer
                 </a>
-              </div>
               </div>
             </div>
           `;
@@ -62,7 +58,7 @@ function updDom(res) {
 
       return `
         <section>
-            <h3>${key.toUpperCase()}</h3>
+            <h3>Type: ${key.toUpperCase()}</h3>
             <div class="row">${animesHTML}</div>
         </section>
       `
